8c0d546324981bc67cbf38e49881bb87e92bb98c
JuanOtavaloMaigua <juanotavalomaigua@gmail.com>
2021-06-01 13:26:47 -0500

    add image in banner to project

--- src/Components/ImageAndText.js
+++ src/Components/ImageAndText.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import "../Styles/Style.css"
 
 export default function ImageAndText(){
     return(
-        <div>
-            <picture className="Image-Container"> 
-                <img src={process.env.PUBLIC_URL + "/images/banner.png"} height="350px"/>
+        <div className="BannerContainer">
+            <picture className="BannerContainer-Picture"> 
+                <source srcSet="https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png" media="(min-width: 500px)"/>
+                <img src={process.env.PUBLIC_URL + "/images/banner.png"}/>
             </picture>
-
+            <div className="BannerContainer-text">
+                <p>Telas</p>
+                <p>Cobijas</p>
+                <p>Insumos</p>
+            </div>
         </div>
     )
 }
---
+++ src/Components/utils/ResponsiveImage.js
@@ -0,0 +1,3 @@
+export function ResponsiveImage(){
+    // This function can make the difference to re use thsi code
+}
